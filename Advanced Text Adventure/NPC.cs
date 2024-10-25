105f4c2ab32f4cd84eac590c83e25ab558de7b60
MUKILTEO\1717222 <tobiahsday@gmail.com>
2024-10-25 13:27:33 -0700

    Emotions

---
+++ Advanced Text Adventure/NPC.cs
@@ -0,0 +1,31 @@
+﻿namespace Advanced_Text_Adventure
+{
+    internal partial class Program
+    {
+        public static NPC practiceManequin = new NPC("Practice Manequin", 0, 0, Emotions.Neutral, Interests.Cotton | Interests.Manequins);
+        public class NPC
+        { 
+            public string name;
+            public int loveMeter;
+            public int seduceAbility;
+            public bool isSwooned;
+            public Emotions emotion;
+            public Interests interests;
+
+            public NPC (string name, int loveMeter, int seduceAbility,  Emotions emotion, Interests interests)
+            {
+                this.name = name;
+                this.loveMeter = loveMeter;
+                this.seduceAbility = seduceAbility;
+                this.isSwooned = false;
+                this.emotion = emotion;
+                this.interests = interests;
+            }
+
+            public bool HasInterests(Interests interestsList)
+            {
+                return interests.HasFlag(interestsList);
+            }
+        }
+    }
+}
--- Advanced Text Adventure/Program.cs
+++ Advanced Text Adventure/Program.cs
@@ -1,11 +1,33 @@
 ﻿namespace Advanced_Text_Adventure
 {
-    internal class Program
+    internal partial class Program
     {
-        abstract class NPC
+        [Flags]
+        public enum Interests 
         { 
-            public string name;
-            
+            Space = 1,
+            Anime = 2,
+            Science = 4,
+            Dogs = 8,
+            Cats = 16,
+            Cotton = 32,
+            Manequins = 64,
+        }
+        public enum Emotions { Neutral, Happy, Sad, Angry, Flustered, Distant, Infatuated}
+
+       
+
+        static T PromptUser<T>()
+        {
+            try
+            {
+                string input = Console.ReadLine();
+                return (T)Convert.ChangeType(input, typeof(T));
+            }
+            catch
+            {
+                return PromptUser<T>();
+            }
         }
     }
 }
